73388823ef075c07c87b71d14f3973d5758f20b3
testing-pandas <seo@jooble.com>
2025-10-28 18:27:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1446,7 +1446,6 @@ app.get('/healthz', (req, res) => {
 app.get('/', (req, res) => {
   const cityCtx = res.locals.cityCtx;
   const activeSiteUrl = ensureAbsoluteUrl(res.locals.activeSiteUrl || MAIN_SITE_URL, CITY_PROTOCOL || SITE_BASE_PARTS.protocol || 'https:');
-  const siteDisplayName = res.locals.siteDisplayName || SITE_NAME;
   const pageSize = 50;
   const cursor = req.query.cursor || '';
   let rows;
